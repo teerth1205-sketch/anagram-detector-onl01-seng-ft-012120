8443e2aa66dfa8497577850622f901a18f359f54
Teerth Patel <teerth1205@gmail.com>
2020-02-02 00:32:29 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def  initialize(word)
+    @word = word
+  end 
+  
+  def match(array)
+    arrays = []
+    array.each do |words|
+     if words.split("").sort == @word.split("").sort
+       arrays  << words
+      end 
+    end 
+    arrays
+  end 
+  
+end 
